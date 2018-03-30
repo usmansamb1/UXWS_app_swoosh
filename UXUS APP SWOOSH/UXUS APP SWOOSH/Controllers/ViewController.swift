d176a19247d363f5f0eea3d9cf648eff7dcd10c7
usman nawaz <usmansamb@gmail.com>
2018-03-31 00:14:32 +0300

    screen one complete1

---
+++ UXUS APP SWOOSH/UXUS APP SWOOSH/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UXUS APP SWOOSH
+//
+//  Created by USMAN NAWAZ on 30/03/2018.
+//  Copyright © 2018 USMAN NAWAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UXUS APP SWOOSH/UXUS APP SWOOSH/Views/BorderBtn.swift
@@ -0,0 +1,30 @@
+//
+//  BorderBtn.swift
+//  UXUS APP SWOOSH
+//
+//  Created by USMAN NAWAZ on 30/03/2018.
+//  Copyright © 2018 USMAN NAWAZ. All rights reserved.
+//
+
+import UIKit
+
+class BorderBtn: UIButton {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.3
+        layer.borderColor = UIColor.white.cgColor
+    layer.cornerRadius = 5.0
+        
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
